412640f4b1b170981644a617a4b93a5a536aa527
ESCRI11 <xavier.escriba.montagut@gmail.com>
2024-09-25 17:23:37 +0200

    fix filter name

--- Exercise-3/dsServer/R/getColnamesDS.R
+++ Exercise-3/dsServer/R/getColnamesDS.R
@@ -1,6 +1,6 @@
 #' @export
 getColnamesDS <- function(table) {
-  myFilter <- getOption("nice.filter")
+  myFilter <- getOption("myFilter")
   n_cols <- ncol(table)
   if (n_cols < myFilter) {
     stop("Number of columns is lower than filter")
